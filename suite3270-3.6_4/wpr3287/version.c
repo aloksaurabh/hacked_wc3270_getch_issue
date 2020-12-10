daebb9a62504ac38ef3981310e8c5b890eb08ffc
aloksaurabh <eye@live.in>
2020-12-11 03:13:43 +0530

    Files

---
+++ suite3270-3.6_4/wpr3287/version.c
@@ -0,0 +1,7 @@
+const char *app = "wpr3287";
+const char *build = "wpr3287 v3.6ga4 Fri Dec 11 01:44:31 2020 eye";
+const char *cyear = "2017";
+const char *app_defaults_version = "3.3.4";
+static char sccsid[] = "@(#)wpr3287 v3.6ga4 2020/12/11 eye";
+const char *build_rpq_timestamp = "202012110144";
+const char *build_rpq_version = "3.6ga4";
